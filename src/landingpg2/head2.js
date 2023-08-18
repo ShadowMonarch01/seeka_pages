ac5ea4b98ba1d8de207ec769aed62f9c3b0518c9
ShadowMonarch01 <gunngptrust@gmail.com>
2023-08-18 14:46:51 +0100

    page 2 mobile

--- src/landingpg2/head2.js
+++ src/landingpg2/head2.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from './l2images/Logo.png'
 import avatar from './l2images/avatar.png'
 import './head2.css'
 
 const Head2 = () => {
+
+  const navRef = useRef();
+
+  const showNavbar = () =>{
+      navRef.current.classList.toggle('responsive_nav');
+  }
 
   let navigate = useNavigate();
 
@@ -21,7 +28,7 @@ const Head2 = () => {
       </div>
       
 
-      <div className="top-btn-wrapper2">
+      <nav ref={navRef} className="top-btn-wrapper2">
             <div onClick={goHome} className="top-btn2 addPointer">
                 <p className="top-btn-txt2">Home</p>
             </div>
@@ -38,7 +45,15 @@ const Head2 = () => {
                 <p className="top-btn-txt2">Sign in</p>
                 <img className="" src={avatar} alt=""/>
             </button>
-        </div>
+
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                <FaTimes/>
+            </button>
+        </nav>
+
+        <button className='nav-btn little-shift' onClick={showNavbar}>
+            <FaBars/>
+        </button>
     </header>
   )
 }
